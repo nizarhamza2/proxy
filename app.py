a74f3ca804f4bcd617fa93790918b7927d900135
nizarhamza2 <nizar.hamza@e-mobility-bs.com>
2025-07-11 23:06:09 +0100

    Fix Flask host and port for Render

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -20,3 +21,8 @@ def post_data():
 @app.route("/", methods=["GET"])
 def index():
     return "Firebase Proxy is running.", 200
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
